34741a80e6ec0b3317caa4c7263942ef40811166
Artyom Parygin <arteme.parygin@yandex.ru>
2023-08-18 21:46:43 +0300

    lesson-01/06 (add array)

--- lesson-01/06.js
+++ lesson-01/06.js
@@ -1,5 +1,11 @@
 let str = "zzzzyzz";
-if (str.includes("y")) {
+
+let arr = [];
+for (let i = 0; i < str.length; i++) {
+    arr.push(str[i]);
+}
+
+if (arr.includes("y")) {
     console.log("нашел");
 } else {
     console.log("заблудился");
