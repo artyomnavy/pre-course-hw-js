8c5942c2c8bd2b3e50b31006a4a81e7d98ddeb6f
Artyom Parygin <arteme.parygin@yandex.ru>
2023-08-19 09:05:34 +0300

    lesson-01/06 (2 line)

--- lesson-01/06.js
+++ lesson-01/06.js
@@ -1,7 +1,2 @@
 let str = "zzzzyzz";
-str = Array.from(str);
-if (str.includes("y")) {
-    console.log("нашел");
-} else {
-    console.log("заблудился");
-}
+console.log(str.includes("y") ? "нашел" : "заблудился");
